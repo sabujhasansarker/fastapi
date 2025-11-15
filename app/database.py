4b1445a9c842f325a26d7d37235ed2d85b8d50c3
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2025-11-16 05:24:43 +0600

    add render api

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:sabujk@localhost/aiquest'
+SQLALCHEMY_DATABASE_URL = 'postgresql://aiquest_mppx_user:gCSo3ckjyZEzokLooTdeClRprRRMjB3m@dpg-d4cgk0mr433s73dhlp1g-a/aiquest_mppx'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

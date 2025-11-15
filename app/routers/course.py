3d66d11dfe7e224a5a2deecfa225601d4aa5099c
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2025-11-15 21:45:04 +0600

    router

---
+++ app/routers/course.py
@@ -0,0 +1,63 @@
+from fastapi import Depends, HTTPException,status,Response, APIRouter
+from sqlalchemy.orm import Session
+from .. import models
+from ..schemas import CourseResponse, CourseCreate
+from ..database import  get_db
+
+router = APIRouter()
+
+# Add course
+@router.post('/course', response_model=CourseResponse)
+def create_course(course: CourseCreate, db: Session = Depends(get_db)):
+    new_course = models.Course(**course.model_dump())
+    db.add(new_course)
+    db.commit()
+    db.refresh(new_course)
+    return new_course
+
+# Get all courses
+@router.get("/courses", response_model=list[CourseResponse])
+def get_courses(db: Session = Depends(get_db)):
+    courses = db.query(models.Course).all()
+    return courses
+
+# get with id
+@router.get("/course/{id}",response_model=CourseResponse)
+def get_course(id:int,db: Session = Depends(get_db)):
+    course= db.query(models.Course).filter(models.Course.id == id).first()
+    if not course:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail=f"course not found id:{id}"
+        )
+    return course
+
+#edit data
+@router.put("/course/{id}",response_model=CourseResponse)
+def update_course(id:int,update_course: CourseCreate,db: Session = Depends(get_db)):
+     course_query= db.query(models.Course).filter(models.Course.id == id)
+     course = course_query.first()
+     if not course:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail=f"course not found id:{id}"
+        )
+     update_data = update_course.model_dump()
+     course_query.update(update_data,synchronize_session=False)
+     db.commit()
+     db.refresh(course)
+     return course
+
+# delete data
+@router.delete("/course/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def get_course(id:int,db: Session = Depends(get_db)):
+    course_query= db.query(models.Course).filter(models.Course.id == id)
+    course= course_query.first()
+    if not course:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail=f"course not found id:{id}"
+        )
+    course_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ app/routers/user.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException,status, APIRouter
+from sqlalchemy.orm import Session
+from .. import models
+from ..schemas import UserCreate, UserResponse
+from ..database import  get_db
+from .. import utils
+
+
+router = APIRouter()
+
+# create user
+@router.post("/user",status_code=status.HTTP_201_CREATED, response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)) :
+    if db.query(models.User).filter(models.User.email == user.email).first() :
+        raise HTTPException(400, "Email already exits")
+    hashed_password = utils.hash_password(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user

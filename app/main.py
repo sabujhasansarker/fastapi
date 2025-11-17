9237d1c64773cd8285b8a48f3454e301204485ad
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2025-11-17 09:28:14 +0600

    add vercel frontend

--- app/main.py
+++ app/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 from .routers import user, course
 from .database import engine, Base
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
 # Auto create tables on startup
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000","https://*.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(course.router)
 app.include_router(user.router)
